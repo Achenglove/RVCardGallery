2ced7813e6fb33f31bf58f7e2c6c22632b992a6c
Acheng <345887272@qq.com>
2018-04-25 17:28:10 +0800

    画廊式RecyclerView

---
+++ app/src/main/java/com/example/ccr/rvcardgallery/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ccr.rvcardgallery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
